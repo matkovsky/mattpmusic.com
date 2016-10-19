c2595bb9e81ec0110f488f08c6d27b7f6c80fc6d
Peter Matkovsky <peter@matkovsky.com>
2016-10-19 19:01:19 +0200

    Add Mixcloud widget to Episodes
    The Mixcloud URL slug needs to be wrapped in `<pre>` tags in the body of a post, and then we will replace those with valid embeddable Mixcloud iframes.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function() {
+
+	var mixcloudElements = document.querySelectorAll('#episodes pre')
+
+	for (var i = mixcloudElements.length - 1; i >= 0; i--) {
+		var mixcloudElement = mixcloudElements[i];
+
+		var mixcloudIframeContainer = document.createElement('div');
+		mixcloudIframeContainer.className = 'mixcloud';
+		mixcloudIframeContainer.innerHTML = '<iframe width="100%" height="120" src="https://www.mixcloud.com/widget/iframe/?feed=' + encodeURIComponent('https://www.mixcloud.com/matkovsky/' + mixcloudElements[i].innerText) + '/&hide_cover=1&hide_artwork=1&light=1" frameborder="0"></iframe>';
+		mixcloudElement.parentNode.replaceChild(mixcloudIframeContainer, mixcloudElement)
+	}
+
+})();
